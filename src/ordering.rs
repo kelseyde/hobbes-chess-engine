8559c2e5ef3697c76cbcef869bd950fcbadd028d
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-06-29 20:01:10 +0200

    Conthist 1 bench 1297886

--- src/ordering.rs
+++ src/ordering.rs
@@ -31,13 +31,17 @@ pub fn score(td: &ThreadData, board: &Board, moves: &MoveList, tt_move: &Move, p
                 }
             } else {
                 let quiet_score = td.quiet_history.get(board.stm, *m) as i32;
-                let cont_score = if let Some(prev_mv) = td.ss[ply - 1].mv {
-                    let pc = board.piece_at(m.from()).unwrap();
-                    if let Some(prev_pc) = td.ss[ply -1].pc {
-                        td.cont_history.get(prev_mv, prev_pc, *m, pc) as i32
+                let cont_score = if ply > 0 {
+                    if let Some(prev_mv) = td.ss[ply - 1].mv {
+                        let pc = board.piece_at(m.from()).unwrap();
+                        if let Some(prev_pc) = td.ss[ply -1].pc {
+                            td.cont_history.get(prev_mv, prev_pc, *m, pc) as i32
+                        } else {
+                            0
+                        }
                     } else {
                         0
-                    }
+                    };
                 } else {
                     0
                 };
