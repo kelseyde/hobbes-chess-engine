c4d123309c118dfe7744d39de1044ded8e7b4e33
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-07-23 19:00:33 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::types::ray;
 use crate::uci::UCI;
 
 pub mod attacks;
@@ -21,11 +22,17 @@ pub mod movepicker;
 pub mod evaluation;
 pub mod parameters;
 pub mod utils;
-mod correction;
+pub mod correction;
 
 fn main() {
+
+    // Initialise static data
+    ray::init();
+
+    // Start up the UCI (Universal Chess Interface)
     let args: Vec<String> = std::env::args().collect();
     UCI::new().run(&args);
+
 }
 
 
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -5,6 +5,7 @@ pub mod rank;
 pub mod piece;
 pub mod side;
 pub mod castling;
+pub mod ray;
 
 pub use file::File;
 pub use rank::Rank;
