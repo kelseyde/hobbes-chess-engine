9bfdf1d181de7c2cfb8cfce424d760d0e4482b28
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-08-09 15:22:41 +0200

    Refactor project structure bench 2680438

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use crate::tools::uci::UCI;
 use board::ray;
 
+/// The board module contains board representation, move generation, move legality checking, and
+/// everything related to the rules of chess.
 pub mod board;
+
+/// The evaluation module contains everything required to interact with the NNUE (Efficiently
+/// Updatable Neural Network), including accumulators, bucket caches, and SIMD operations.
 pub mod evaluation;
+
+/// The search module contains the search algorithm, move ordering heuristics, transposition and
+/// history tables, and everything required to traverse the game tree.
 pub mod search;
+
+/// The tools module contains various utilities not strictly related to the engine itself, including
+/// perft, datagen, fen and scharnagl parsing, and UCI (Universal Chess Interface) support.
 pub mod tools;
 
 fn main() {
