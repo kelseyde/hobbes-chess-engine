c6e8f766f2ce3ecdc17b6d10507be679cd711b84
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-09-19 14:30:30 +0200

    Add pretty uci info bench 2512864

--- src/tools/pretty.rs
+++ src/tools/pretty.rs
@@ -1,15 +1,19 @@
-use crate::{AUTHOR, VERSION};
+use crate::{AUTHOR, CONTRIBUTORS, VERSION};
 
-const RED: &str = "\x1b[31m";
-const ORANGE: &str = "\x1b[38;2;255;165;0m";
+const CYAN: &str = "\x1b[36m";
 const RESET: &str = "\x1b[0m";
 
 pub fn print_uci_info() {
-    let version_formatted = format!("[{ORANGE}{VERSION}{RESET}]");
-    let author_formatted = format!("{ORANGE}{}{}", AUTHOR, RESET);
-    println!("┌──────────────────────┐");
-    println!("│ Hobbes               │");
-    println!("│ Version: {:<33}│", version_formatted);
-    println!("│ Author: {:<34}│", author_formatted);
-    println!("└──────────────────────┘");
+    let version_formatted = format!("{CYAN}{VERSION}{RESET}");
+    let author_formatted = format!("{CYAN}{}{}", AUTHOR, RESET);
+    let contributors_formatted = format!("{CYAN}{}{}", CONTRIBUTORS, RESET);
+    println!("┌───────────────────────────────────────────────────────────────────┐");
+    println!("│ Hobbes                                                            │");
+    println!("│ Version: {:<66}│", version_formatted);
+    println!("│ Author: {:<67}│", author_formatted);
+    println!("│ Contributors: {:<61}│", contributors_formatted);
+    println!("│ OpenBench: CalvinBench https://kelseyde.pythonanywhere.com/index/ │");
+    println!("│            MattBench https://chess.n9x.co/index/                  │");
+    println!("└───────────────────────────────────────────────────────────────────┘");
+    println!("Type 'help' for a list of commands.");
 }
