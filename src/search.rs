ef1a0eeb800d5f26b69ea61b2625be3337faebd9
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-09-22 14:26:26 +0200

    rfp cutnode bench 2244919

--- src/search.rs
+++ src/search.rs
@@ -284,6 +284,7 @@ fn alpha_beta(board: &Board,
         let futility_margin = rfp_base()
             + rfp_scale() * depth
             - rfp_improving_scale() * improving as i32
+            - rfp_cutnode_scale() * cut_node as i32
             - rfp_tt_move_noisy_scale() * tt_move_noisy as i32;
         if depth <= rfp_max_depth() && static_eval - futility_margin >= beta {
             return beta + (static_eval - beta) / 3;
