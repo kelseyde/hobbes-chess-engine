f761b57b66e32b491f6a5978686057b28b460f8a
Dan Kelsey <daniel.kelsey@hotmail.co.uk>
2025-09-22 07:55:46 +0200

    Reduce minimum LMR depth (#181)
    bench 2306822

--- src/search/parameters.rs
+++ src/search/parameters.rs
@@ -54,11 +54,12 @@ tunable_params! {
     se_depth_offset             = 1, 0, 3, 1;
     se_depth_divisor            = 2, 1, 4, 1;
     se_double_ext_margin        = 17, 10, 30, 5;
-    lmr_min_depth               = 3, 1, 5, 1;
+    lmr_min_depth               = 2, 1, 5, 1;
     lmr_min_moves               = 3, 1, 4, 1;
     lmr_pv_node                 = 1124, 0, 2048, 256;
     lmr_cut_node                = 1398, 0, 2048, 256;
     lmr_improving               = 629, 0, 2048, 256;
+    lmr_shallow                 = 1024, 0, 2048, 256;
     lmr_hist_offset             = 387, -2048, 2048, 256;
     lmr_hist_divisor            = 19156, 8192, 32768, 2048;
     lmr_cont_hist_bonus_scale   = 209, 80, 280, 40;
