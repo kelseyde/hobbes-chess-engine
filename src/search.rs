b3b3ebd71bd3fd4aabf151f1101f38ac5dddc061
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-09-22 17:04:19 +0200

    No rfp if upper ttflag bench 2447113

--- src/search.rs
+++ src/search.rs
@@ -285,7 +285,9 @@ fn alpha_beta(board: &Board,
             + rfp_scale() * depth
             - rfp_improving_scale() * improving as i32
             - rfp_tt_move_noisy_scale() * tt_move_noisy as i32;
-        if depth <= rfp_max_depth() && static_eval - futility_margin >= beta {
+        if depth <= rfp_max_depth()
+            && static_eval - futility_margin >= beta
+            && tt_flag != TTFlag::Upper {
             return beta + (static_eval - beta) / 3;
         }
 
