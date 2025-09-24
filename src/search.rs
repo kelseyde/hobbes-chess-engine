6ffe5ad910f02957e6af4b9328728f5f231c8e92
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-09-24 20:55:42 +0200

    thing bench 2046622

--- src/search.rs
+++ src/search.rs
@@ -445,7 +445,9 @@ fn alpha_beta(board: &Board,
         // SEE Pruning
         // Skip moves that lose material once all the pieces have been exchanged.
         let see_threshold = if is_quiet {
-            (pvs_see_quiet_scale() * depth) - history_score / pvs_see_quiet_history_div()
+            (pvs_see_quiet_scale() * depth)
+                - history_score / pvs_see_quiet_history_div()
+                - 12 * lmr_depth * tt_pv as i32
         } else {
             (pvs_see_noisy_scale() * depth * depth) - history_score / pvs_see_noisy_history_div()
         };
