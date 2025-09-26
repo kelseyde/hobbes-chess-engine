5208c4af9976ac79583c1f62796bb1453a7ce811
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-09-26 20:31:37 +0200

    hp8 bench 2308419

--- src/search/parameters.rs
+++ src/search/parameters.rs
@@ -39,7 +39,7 @@ tunable_params! {
     lmp_improving_base          = 3, 1, 5, 1;
     lmp_scale                   = 38, 10, 100, 20;
     lmp_improving_scale         = 89, 10, 100, 20;
-    hp_max_depth                = 4, 4, 8, 1;
+    hp_max_depth                = 8, 4, 8, 1;
     hp_scale                    = -2111, -3072, -1024, 128;
     bnp_max_depth               = 6, 4, 10, 1;
     bnp_scale                   = 122, 64, 256, 16;
