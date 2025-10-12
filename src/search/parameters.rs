3f83d15fae9d82db4df5cb6d8b35ad80f5ab4026
Dan Kelsey <daniel.kelsey@hotmail.co.uk>
2025-10-12 16:55:18 +0200

    Add NMP eval margin (#201)
    bench 3160899

--- src/search/parameters.rs
+++ src/search/parameters.rs
@@ -14,6 +14,7 @@ tunable_params! {
     razor_base                  = 301, 200, 500, 25;
     razor_scale                 = 273, 100, 400, 25;
     nmp_min_depth               = 3, 0, 8, 1;
+    nmp_margin                  = 30, 0, 80, 10;
     nmp_base_reduction          = 4, 2, 5, 1;
     nmp_depth_divisor           = 3, 1, 4, 1;
     nmp_eval_divisor            = 213, 100, 300, 25;
