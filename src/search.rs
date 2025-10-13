ba9c968ff2628a5864f99d249474a6e878ff885b
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-10-13 15:48:02 +0200

    both multicuts bench 2791849

--- src/search.rs
+++ src/search.rs
@@ -482,6 +482,8 @@ fn alpha_beta(board: &Board,
             if score < s_beta {
                 extension = 1;
                 extension += (!pv_node && score < s_beta - se_double_ext_margin()) as i32;
+            } else if score >= beta {
+                return score;
             } else if s_beta >= beta {
                 return s_beta;
             } else if tt_score >= beta {
