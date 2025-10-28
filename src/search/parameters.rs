6f7f4979d31421d07f8cb53935c18ab39313e887
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-10-28 13:57:53 +0100

    Triple ext bench 1739969

--- src/search/parameters.rs
+++ src/search/parameters.rs
@@ -56,7 +56,7 @@ tunable_params! {
     se_depth_offset             = 1, 0, 3, 1;
     se_depth_divisor            = 2, 1, 4, 1;
     se_double_ext_margin        = 12, 10, 30, 5;
-    se_triple_ext_margin        = 80, 50, 150, 10;
+    se_triple_ext_margin        = 180, 50, 200, 10;
     lmr_min_depth               = 2, 1, 5, 1;
     lmr_min_moves               = 2, 1, 4, 1;
     lmr_ttpv_base               = 567, 0, 2048, 256;
