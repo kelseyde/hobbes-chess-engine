42c815deb96275c41c7ca04bff7085c0a4b41263
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-10-28 14:49:15 +0100

    triple ext bench 1963265

--- src/search.rs
+++ src/search.rs
@@ -487,7 +487,7 @@ fn alpha_beta(board: &Board,
             if score < s_beta {
                 extension = 1;
                 extension += (!pv_node && score < s_beta - se_double_ext_margin()) as i32;
-                extension += (!pv_node && score < s_beta - se_triple_ext_margin()) as i32;
+                extension += (!pv_node && is_quiet && score < s_beta - se_triple_ext_margin()) as i32;
             } else if s_beta >= beta {
                 return s_beta;
             } else if tt_score >= beta {
