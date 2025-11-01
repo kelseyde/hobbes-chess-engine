01820923176ed2e02134df3b4fcd83a8f2e1b549
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-11-01 12:06:20 +0100

    WIP

--- src/evaluation/network.rs
+++ src/evaluation/network.rs
@@ -1,3 +1,4 @@
+use std::ops::{Deref, DerefMut};
 use crate::evaluation::NUM_BUCKETS;
 
 pub const FEATURES: usize = 768;
@@ -11,11 +12,31 @@ pub(crate) static NETWORK: Network =
     unsafe { std::mem::transmute(*include_bytes!("../../hobbes.nnue")) };
 
 pub type FeatureWeights = [i16; FEATURES * HIDDEN];
+pub type Block = [i16; HIDDEN];
 
 #[repr(C, align(64))]
 pub struct Network {
     pub feature_weights: [FeatureWeights; NUM_BUCKETS],
-    pub feature_bias: [i16; HIDDEN],
-    pub output_weights: [[i16; HIDDEN]; 2],
+    pub feature_bias: Block,
+    pub output_weights: [Block; 2],
     pub output_bias: i16,
+}
+
+
+#[repr(C, align(64))]
+#[derive(Clone, Copy, Debug, PartialOrd, PartialEq)]
+pub struct Align64<T>(pub T);
+
+impl<T, const N: usize> Deref for Align64<[T; N]> {
+    type Target = [T; N];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T, const N: usize> DerefMut for Align64<[T; N]> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
 }
