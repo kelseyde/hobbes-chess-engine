4bfb532205a07b1d8a652d30b6287d49be6bf890
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-11-16 14:03:46 +0100

    WIP

--- src/evaluation/accumulator.rs
+++ src/evaluation/accumulator.rs
@@ -119,6 +119,42 @@ impl Accumulator {
         }
     }
 
+    #[inline]
+    pub fn add(&mut self, add: Feature, weights: &FeatureWeights, perspective: Side) {
+        let mirror = unsafe { *self.mirrored.get_unchecked(perspective as usize) };
+        let idx = add.index(perspective, mirror);
+        let feats = self.features_mut(perspective);
+        let weight_offset = idx * HIDDEN;
+
+        let mut i = 0;
+        while i < HIDDEN {
+            unsafe {
+                let feat_ptr = feats.get_unchecked_mut(i);
+                let weight = *weights.get_unchecked(i + weight_offset);
+                *feat_ptr = feat_ptr.wrapping_add(weight);
+            }
+            i += 1;
+        }
+    }
+
+    #[inline]
+    pub fn sub(&mut self, sub: Feature, weights: &FeatureWeights, perspective: Side) {
+        let mirror = unsafe { *self.mirrored.get_unchecked(perspective as usize) };
+        let idx = sub.index(perspective, mirror);
+        let feats = self.features_mut(perspective);
+        let weight_offset = idx * HIDDEN;
+
+        let mut i = 0;
+        while i < HIDDEN {
+            unsafe {
+                let feat_ptr = feats.get_unchecked_mut(i);
+                let weight = *weights.get_unchecked(i + weight_offset);
+                *feat_ptr = feat_ptr.wrapping_sub(weight);
+            }
+            i += 1;
+        }
+    }
+
 }
 
 pub fn apply_update(
@@ -182,6 +218,71 @@ pub fn add(
             *out_feat_ptr = in_feat_ptr.wrapping_add(weight);
         }
         i += 1;
+    }
+
+    #[inline]
+    pub fn add_add_add_add(
+        &mut self,
+        add1: Feature,
+        add2: Feature,
+        add3: Feature,
+        add4: Feature,
+        weights: &FeatureWeights,
+        perspective: Side,
+    ) {
+        let mirror = self.mirrored[perspective as usize];
+
+        let add1_offset = add1.index(perspective, mirror) * HIDDEN;
+        let add2_offset = add2.index(perspective, mirror) * HIDDEN;
+        let add3_offset = add3.index(perspective, mirror) * HIDDEN;
+        let add4_offset = add4.index(perspective, mirror) * HIDDEN;
+
+        let feats = self.features_mut(perspective);
+
+        let mut i = 0;
+        while i < HIDDEN {
+            unsafe {
+                let feat_ptr = feats.get_unchecked_mut(i);
+                *feat_ptr = feat_ptr
+                    .wrapping_add(*weights.get_unchecked(i + add1_offset))
+                    .wrapping_add(*weights.get_unchecked(i + add2_offset))
+                    .wrapping_add(*weights.get_unchecked(i + add3_offset))
+                    .wrapping_add(*weights.get_unchecked(i + add4_offset));
+            }
+            i += 1;
+        }
+    }
+
+    pub fn sub_sub_sub_sub(
+        &mut self,
+        sub1: Feature,
+        sub2: Feature,
+        sub3: Feature,
+        sub4: Feature,
+        weights: &FeatureWeights,
+        perspective: Side,
+    ) {
+        let mirror = self.mirrored[perspective as usize];
+
+        let sub1_offset = sub1.index(perspective, mirror) * HIDDEN;
+        let sub2_offset = sub2.index(perspective, mirror) * HIDDEN;
+        let sub3_offset = sub3.index(perspective, mirror) * HIDDEN;
+        let sub4_offset = sub4.index(perspective, mirror) * HIDDEN;
+
+        let feats = self.features_mut(perspective);
+
+        let mut i = 0;
+        while i < HIDDEN {
+            unsafe {
+                let feat_ptr = feats.get_unchecked_mut(i);
+                *feat_ptr = feat_ptr
+                    .wrapping_sub(*weights.get_unchecked(i + sub1_offset))
+                    .wrapping_sub(*weights.get_unchecked(i + sub2_offset))
+                    .wrapping_sub(*weights.get_unchecked(i + sub3_offset))
+                    .wrapping_sub(*weights.get_unchecked(i + sub4_offset));
+            }
+            i += 1;
+        }
     }
 }
 
