f94546cf61b92e31dbac2adbe8e936670720b59c
Dan Kelsey <daniel.kelsey@hotmail.co.uk>
2025-11-16 15:57:53 +0100

    Lazy NNUE updates (#237)
    bench 1673612

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -12,10 +12,10 @@ use crate::board::side::Side;
 use crate::board::side::Side::{Black, White};
 use crate::board::square::Square;
 use crate::board::{castling, Board};
-use crate::evaluation::accumulator::Accumulator;
+use crate::evaluation::accumulator::{Accumulator, AccumulatorUpdate};
 use crate::evaluation::cache::InputBucketCache;
 use crate::evaluation::feature::Feature;
-use crate::evaluation::network::{FeatureWeights, HIDDEN, NETWORK, QA, QAB, SCALE};
+use crate::evaluation::network::{HIDDEN, NETWORK, QA, QAB, SCALE};
 use crate::search::parameters::{
     material_scaling_base, scale_value_bishop, scale_value_knight, scale_value_queen,
     scale_value_rook,
@@ -63,6 +63,7 @@ impl NNUE {
     /// sets, based on the side to move. Then, passes the features through the network to get the
     /// static evaluation.
     pub fn evaluate(&mut self, board: &Board) -> i32 {
+        self.apply_lazy_updates(board);
         let acc = &self.stack[self.current];
         let us = match board.stm {
             White => &acc.white_features,
@@ -180,6 +181,9 @@ impl NNUE {
             acc.sub(sub, weights, perspective);
         }
 
+        acc.computed[perspective] = true;
+        acc.needs_refresh[perspective] = false;
+
         cache_entry.bitboards = board.bb;
         cache_entry.features = *acc.features(perspective);
     }
@@ -189,45 +193,82 @@ impl NNUE {
     /// updated.
     pub fn update(&mut self, mv: &Move, pc: Piece, captured: Option<Piece>, board: &Board) {
         self.current += 1;
-        // TODO: This can be optimized. No need to have an extra copy
-        self.stack[self.current] = self.stack[self.current - 1];
+        self.stack[self.current].needs_refresh = self.stack[self.current - 1].needs_refresh;
+        self.stack[self.current].mirrored = self.stack[self.current - 1].mirrored;
+        self.stack[self.current].computed[White] = false;
+        self.stack[self.current].computed[Black] = false;
         let us = board.stm;
 
-        let new_pc = if let Some(promo_pc) = mv.promo_piece() {
-            promo_pc
-        } else {
-            pc
-        };
-
-        let w_king_sq = king_square(board, *mv, new_pc, White);
-        let b_king_sq = king_square(board, *mv, new_pc, Black);
-
-        let w_bucket = king_bucket(w_king_sq, White);
-        let b_bucket = king_bucket(b_king_sq, Black);
-
-        let w_weights = &NETWORK.feature_weights[w_bucket];
-        let b_weights = &NETWORK.feature_weights[b_bucket];
-
+        let new_pc = mv.promo_piece().unwrap_or(pc);
         let mirror_changed = mirror_changed(board, *mv, new_pc);
         let bucket_changed = bucket_changed(board, *mv, new_pc, us);
         let refresh_required = mirror_changed || bucket_changed;
 
         if refresh_required {
-            let bucket = if us == White { w_bucket } else { b_bucket };
-            let mut mirror = should_mirror(board.king_sq(us));
-            if mirror_changed {
-                mirror = !mirror
-            }
-            self.full_refresh(board, self.current, us, mirror, bucket);
+            self.stack[self.current].needs_refresh[us] = true;
         }
 
-        if mv.is_castle() {
-            self.handle_castle(board, mv, us, w_weights, b_weights);
+        self.stack[self.current].update = if mv.is_castle() {
+            self.handle_castle(board, mv, us)
         } else if let Some(captured) = captured {
-            self.handle_capture(mv, pc, new_pc, captured, us, w_weights, b_weights);
+            self.handle_capture(mv, pc, new_pc, captured, us)
         } else {
-            self.handle_standard(mv, pc, new_pc, us, w_weights, b_weights);
+            self.handle_standard(mv, pc, new_pc, us)
         };
+    }
+
+    fn apply_lazy_updates(&mut self, board: &Board) {
+        for perspective in [White, Black] {
+            if self.stack[self.current].computed[perspective] {
+                continue; // already up-to-date for this perspective
+            }
+
+            let king_sq = board.king_sq(perspective);
+            let mirror = should_mirror(king_sq);
+            let bucket = king_bucket(king_sq, perspective);
+
+            if self.stack[self.current].needs_refresh[perspective] {
+                self.full_refresh(board, self.current, perspective, mirror, bucket);
+                continue;
+            }
+
+            // Scan backwards to find the nearest parent accumulator that is computed
+            // for this perspective.
+            let mut curr = self.current - 1;
+            while !self.stack[curr].computed[perspective]
+                && !self.stack[curr].needs_refresh[perspective]
+            {
+                if curr == 0 {
+                    break;
+                }
+                curr -= 1;
+            }
+
+            if self.stack[curr].needs_refresh[perspective] {
+                self.full_refresh(board, self.current, perspective, mirror, bucket);
+            } else {
+                let weights = &NETWORK.feature_weights[bucket];
+                // Apply all updates from that accumulator up to the current one
+                while curr < self.current {
+                    let (front, back) = self.stack.split_at_mut(curr + 1);
+                    let prev_acc = front.last().unwrap();
+                    let next_acc = back.first_mut().unwrap();
+                    let update = next_acc.update;
+                    let prev_features = prev_acc.features(perspective);
+                    let next_features = next_acc.features_mut(perspective);
+                    accumulator::apply_update(
+                        prev_features,
+                        next_features,
+                        weights,
+                        &update,
+                        perspective,
+                        mirror,
+                    );
+                    next_acc.computed[perspective] = true;
+                    curr += 1;
+                }
+            }
+        }
     }
 
     /// Update the accumulator for a standard move (no castle or capture). The old piece is removed
@@ -239,13 +280,14 @@ impl NNUE {
         pc: Piece,
         new_pc: Piece,
         side: Side,
-        w_weights: &FeatureWeights,
-        b_weights: &FeatureWeights,
-    ) {
+    ) -> AccumulatorUpdate {
         let pc_ft = Feature::new(pc, mv.from(), side);
         let new_pc_ft = Feature::new(new_pc, mv.to(), side);
 
-        self.stack[self.current].add_sub(new_pc_ft, pc_ft, w_weights, b_weights);
+        let mut update = AccumulatorUpdate::default();
+        update.push_sub(pc_ft);
+        update.push_add(new_pc_ft);
+        update
     }
 
     /// Update the accumulator for a capture move. The old piece is removed from the starting
@@ -259,9 +301,7 @@ impl NNUE {
         new_pc: Piece,
         captured: Piece,
         side: Side,
-        w_weights: &FeatureWeights,
-        b_weights: &FeatureWeights,
-    ) {
+    ) -> AccumulatorUpdate {
         let capture_sq = if mv.is_ep() {
             Square(mv.to().0 ^ 8)
         } else {
@@ -272,19 +312,16 @@ impl NNUE {
         let new_pc_ft = Feature::new(new_pc, mv.to(), side);
         let capture_ft = Feature::new(captured, capture_sq, !side);
 
-        self.stack[self.current].add_sub_sub(new_pc_ft, pc_ft, capture_ft, w_weights, b_weights);
+        let mut update = AccumulatorUpdate::default();
+        update.push_sub(pc_ft);
+        update.push_add(new_pc_ft);
+        update.push_sub(capture_ft);
+        update
     }
 
     /// Update the accumulator for a castling move. The king and rook are moved to their new
     /// positions, and the old positions are cleared.
-    fn handle_castle(
-        &mut self,
-        board: &Board,
-        mv: &Move,
-        us: Side,
-        w_weights: &FeatureWeights,
-        b_weights: &FeatureWeights,
-    ) {
+    fn handle_castle(&mut self, board: &Board, mv: &Move, us: Side) -> AccumulatorUpdate {
         let kingside = mv.to().0 > mv.from().0;
         let king_from = mv.from();
         let king_to = if board.is_frc() {
@@ -299,36 +336,22 @@ impl NNUE {
         };
         let rook_to = castling::rook_to(us, kingside);
 
-        let king_from_ft = Feature::new(Piece::King, king_from, us);
-        let king_to_ft = Feature::new(Piece::King, king_to, us);
-        let rook_from_ft = Feature::new(Piece::Rook, rook_from, us);
-        let rook_to_ft = Feature::new(Piece::Rook, rook_to, us);
+        let king_from_ft = Feature::new(King, king_from, us);
+        let king_to_ft = Feature::new(King, king_to, us);
+        let rook_from_ft = Feature::new(Rook, rook_from, us);
+        let rook_to_ft = Feature::new(Rook, rook_to, us);
 
-        self.stack[self.current].add_add_sub_sub(
-            king_to_ft,
-            rook_to_ft,
-            king_from_ft,
-            rook_from_ft,
-            w_weights,
-            b_weights,
-        );
+        let mut update = AccumulatorUpdate::default();
+        update.push_sub(king_from_ft);
+        update.push_add(king_to_ft);
+        update.push_sub(rook_from_ft);
+        update.push_add(rook_to_ft);
+        update
     }
 
     /// Undo the last move by decrementing the current accumulator index.
     pub fn undo(&mut self) {
         self.current = self.current.saturating_sub(1);
-    }
-}
-
-#[inline]
-fn king_square(board: &Board, mv: Move, pc: Piece, side: Side) -> Square {
-    if side != board.stm || pc != King {
-        board.king_sq(side)
-    } else if mv.is_castle() && board.is_frc() {
-        let kingside = castling::is_kingside(mv.from(), mv.to());
-        castling::king_to(board.stm, kingside)
-    } else {
-        mv.to()
     }
 }
 
@@ -399,4 +422,35 @@ pub const fn get_num_buckets<const N: usize>(arr: &[usize; N]) -> usize {
         i += 1;
     }
     max + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tools::fen;
+
+    #[test]
+    fn test_lazy_updates() {
+        let mut nnue1 = NNUE::default();
+        let mut nnue2 = NNUE::default();
+
+        let mut board = Board::from_fen(fen::STARTPOS).unwrap();
+        nnue1.activate(&board);
+        nnue2.activate(&board);
+
+        let eval1 = nnue1.evaluate(&board);
+        let eval2 = nnue2.evaluate(&board);
+        assert_eq!(eval1, eval2);
+
+        let mv = Move::parse_uci("e2e4");
+        let pc = Pawn;
+        nnue1.update(&mv, pc, None, &board);
+        board.make(&mv);
+
+        nnue2.activate(&board);
+
+        let eval1 = nnue1.evaluate(&board);
+        let eval2 = nnue2.evaluate(&board);
+        assert_eq!(eval1, eval2);
+    }
 }
--- src/evaluation/accumulator.rs
+++ src/evaluation/accumulator.rs
@@ -8,7 +8,28 @@ use crate::evaluation::network::{FeatureWeights, HIDDEN, NETWORK};
 pub struct Accumulator {
     pub white_features: [i16; HIDDEN],
     pub black_features: [i16; HIDDEN],
+    pub update: AccumulatorUpdate,
+    pub computed: [bool; 2],
+    pub needs_refresh: [bool; 2],
     pub mirrored: [bool; 2],
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct AccumulatorUpdate {
+    pub add_count: usize,
+    pub sub_count: usize,
+    pub adds: [Option<Feature>; 2],
+    pub subs: [Option<Feature>; 2],
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum AccumulatorUpdateType {
+    None,
+    Add,
+    Sub,
+    AddSub,
+    AddSubSub,
+    AddAddSubSub,
 }
 
 impl Default for Accumulator {
@@ -16,7 +37,38 @@ impl Default for Accumulator {
         Accumulator {
             white_features: NETWORK.feature_bias,
             black_features: NETWORK.feature_bias,
+            update: AccumulatorUpdate::default(),
+            computed: [false, false],
+            needs_refresh: [false, false],
             mirrored: [false, false],
+        }
+    }
+}
+
+impl AccumulatorUpdate {
+    pub fn push_add(&mut self, feature: Feature) {
+        if self.add_count < 2 {
+            self.adds[self.add_count] = Some(feature);
+            self.add_count += 1;
+        }
+    }
+
+    pub fn push_sub(&mut self, feature: Feature) {
+        if self.sub_count < 2 {
+            self.subs[self.sub_count] = Some(feature);
+            self.sub_count += 1;
+        }
+    }
+
+    pub fn update_type(&self) -> AccumulatorUpdateType {
+        match (self.add_count, self.sub_count) {
+            (0, 0) => AccumulatorUpdateType::None,
+            (1, 0) => AccumulatorUpdateType::Add,
+            (0, 1) => AccumulatorUpdateType::Sub,
+            (1, 1) => AccumulatorUpdateType::AddSub,
+            (1, 2) => AccumulatorUpdateType::AddSubSub,
+            (2, 2) => AccumulatorUpdateType::AddAddSubSub,
+            _ => AccumulatorUpdateType::None,
         }
     }
 }
@@ -27,6 +79,14 @@ impl Accumulator {
         match perspective {
             White => &self.white_features,
             Black => &self.black_features,
+        }
+    }
+
+    #[inline(always)]
+    pub fn features_mut(&mut self, perspective: Side) -> &mut [i16; HIDDEN] {
+        match perspective {
+            White => &mut self.white_features,
+            Black => &mut self.black_features,
         }
     }
 
@@ -44,14 +104,6 @@ impl Accumulator {
         match side {
             White => self.white_features = *features,
             Black => self.black_features = *features,
-        }
-    }
-
-    #[inline(always)]
-    fn features_mut(&mut self, perspective: Side) -> &mut [i16; HIDDEN] {
-        match perspective {
-            White => &mut self.white_features,
-            Black => &mut self.black_features,
         }
     }
 
@@ -86,120 +138,6 @@ impl Accumulator {
                 let feat_ptr = feats.get_unchecked_mut(i);
                 let weight = *weights.get_unchecked(i + weight_offset);
                 *feat_ptr = feat_ptr.wrapping_sub(weight);
-            }
-            i += 1;
-        }
-    }
-
-    #[inline]
-    pub fn add_sub(
-        &mut self,
-        add: Feature,
-        sub: Feature,
-        w_weights: &FeatureWeights,
-        b_weights: &FeatureWeights,
-    ) {
-        let w_mirror = self.mirrored[0];
-        let b_mirror = self.mirrored[1];
-
-        let w_add_offset = add.index(White, w_mirror) * HIDDEN;
-        let b_add_offset = add.index(Black, b_mirror) * HIDDEN;
-        let w_sub_offset = sub.index(White, w_mirror) * HIDDEN;
-        let b_sub_offset = sub.index(Black, b_mirror) * HIDDEN;
-
-        let mut i = 0;
-        while i < HIDDEN {
-            unsafe {
-                let w_feat_ptr = self.white_features.get_unchecked_mut(i);
-                *w_feat_ptr = w_feat_ptr
-                    .wrapping_add(*w_weights.get_unchecked(i + w_add_offset))
-                    .wrapping_sub(*w_weights.get_unchecked(i + w_sub_offset));
-
-                let b_feat_ptr = self.black_features.get_unchecked_mut(i);
-                *b_feat_ptr = b_feat_ptr
-                    .wrapping_add(*b_weights.get_unchecked(i + b_add_offset))
-                    .wrapping_sub(*b_weights.get_unchecked(i + b_sub_offset));
-            }
-            i += 1;
-        }
-    }
-
-    #[inline]
-    pub fn add_sub_sub(
-        &mut self,
-        add: Feature,
-        sub1: Feature,
-        sub2: Feature,
-        w_weights: &FeatureWeights,
-        b_weights: &FeatureWeights,
-    ) {
-        let w_mirror = self.mirrored[0];
-        let b_mirror = self.mirrored[1];
-
-        let w_add_offset = add.index(White, w_mirror) * HIDDEN;
-        let b_add_offset = add.index(Black, b_mirror) * HIDDEN;
-        let w_sub1_offset = sub1.index(White, w_mirror) * HIDDEN;
-        let b_sub1_offset = sub1.index(Black, b_mirror) * HIDDEN;
-        let w_sub2_offset = sub2.index(White, w_mirror) * HIDDEN;
-        let b_sub2_offset = sub2.index(Black, b_mirror) * HIDDEN;
-
-        let mut i = 0;
-        while i < HIDDEN {
-            unsafe {
-                let w_feat_ptr = self.white_features.get_unchecked_mut(i);
-                *w_feat_ptr = w_feat_ptr
-                    .wrapping_add(*w_weights.get_unchecked(i + w_add_offset))
-                    .wrapping_sub(*w_weights.get_unchecked(i + w_sub1_offset))
-                    .wrapping_sub(*w_weights.get_unchecked(i + w_sub2_offset));
-
-                let b_feat_ptr = self.black_features.get_unchecked_mut(i);
-                *b_feat_ptr = b_feat_ptr
-                    .wrapping_add(*b_weights.get_unchecked(i + b_add_offset))
-                    .wrapping_sub(*b_weights.get_unchecked(i + b_sub1_offset))
-                    .wrapping_sub(*b_weights.get_unchecked(i + b_sub2_offset));
-            }
-            i += 1;
-        }
-    }
-
-    #[inline]
-    pub fn add_add_sub_sub(
-        &mut self,
-        add1: Feature,
-        add2: Feature,
-        sub1: Feature,
-        sub2: Feature,
-        w_weights: &FeatureWeights,
-        b_weights: &FeatureWeights,
-    ) {
-        let w_mirror = self.mirrored[0];
-        let b_mirror = self.mirrored[1];
-
-        let w_add1_offset = add1.index(White, w_mirror) * HIDDEN;
-        let b_add1_offset = add1.index(Black, b_mirror) * HIDDEN;
-        let w_add2_offset = add2.index(White, w_mirror) * HIDDEN;
-        let b_add2_offset = add2.index(Black, b_mirror) * HIDDEN;
-        let w_sub1_offset = sub1.index(White, w_mirror) * HIDDEN;
-        let b_sub1_offset = sub1.index(Black, b_mirror) * HIDDEN;
-        let w_sub2_offset = sub2.index(White, w_mirror) * HIDDEN;
-        let b_sub2_offset = sub2.index(Black, b_mirror) * HIDDEN;
-
-        let mut i = 0;
-        while i < HIDDEN {
-            unsafe {
-                let w_feat_ptr = self.white_features.get_unchecked_mut(i);
-                *w_feat_ptr = w_feat_ptr
-                    .wrapping_add(*w_weights.get_unchecked(i + w_add1_offset))
-                    .wrapping_add(*w_weights.get_unchecked(i + w_add2_offset))
-                    .wrapping_sub(*w_weights.get_unchecked(i + w_sub1_offset))
-                    .wrapping_sub(*w_weights.get_unchecked(i + w_sub2_offset));
-
-                let b_feat_ptr = self.black_features.get_unchecked_mut(i);
-                *b_feat_ptr = b_feat_ptr
-                    .wrapping_add(*b_weights.get_unchecked(i + b_add1_offset))
-                    .wrapping_add(*b_weights.get_unchecked(i + b_add2_offset))
-                    .wrapping_sub(*b_weights.get_unchecked(i + b_sub1_offset))
-                    .wrapping_sub(*b_weights.get_unchecked(i + b_sub2_offset));
             }
             i += 1;
         }
@@ -268,5 +206,294 @@ impl Accumulator {
             }
             i += 1;
         }
+    }
+}
+
+pub fn apply_update(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    weights: &FeatureWeights,
+    update: &AccumulatorUpdate,
+    perspective: Side,
+    mirror: bool,
+) {
+    match update.update_type() {
+        AccumulatorUpdateType::None => {}
+        AccumulatorUpdateType::Add => {
+            if let Some(add1) = update.adds[0] {
+                add(
+                    input_features,
+                    output_features,
+                    add1,
+                    weights,
+                    perspective,
+                    mirror,
+                );
+            }
+        }
+        AccumulatorUpdateType::Sub => {
+            if let Some(sub1) = update.subs[0] {
+                sub(
+                    input_features,
+                    output_features,
+                    sub1,
+                    weights,
+                    perspective,
+                    mirror,
+                );
+            }
+        }
+        AccumulatorUpdateType::AddSub => {
+            if let (Some(add1), Some(sub1)) = (update.adds[0], update.subs[0]) {
+                add_sub(
+                    input_features,
+                    output_features,
+                    add1,
+                    sub1,
+                    weights,
+                    perspective,
+                    mirror,
+                );
+            }
+        }
+        AccumulatorUpdateType::AddSubSub => {
+            if let (Some(add), Some(sub1), Some(sub2)) =
+                (update.adds[0], update.subs[0], update.subs[1])
+            {
+                add_sub_sub(
+                    input_features,
+                    output_features,
+                    add,
+                    sub1,
+                    sub2,
+                    weights,
+                    perspective,
+                    mirror,
+                );
+            }
+        }
+        AccumulatorUpdateType::AddAddSubSub => {
+            if let (Some(add1), Some(add2), Some(sub1), Some(sub2)) = (
+                update.adds[0],
+                update.adds[1],
+                update.subs[0],
+                update.subs[1],
+            ) {
+                add_add_sub_sub(
+                    input_features,
+                    output_features,
+                    add1,
+                    add2,
+                    sub1,
+                    sub2,
+                    weights,
+                    perspective,
+                    mirror,
+                );
+            }
+        }
+    }
+}
+
+#[inline]
+pub fn add(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    add: Feature,
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let idx = add.index(perspective, mirror);
+    let weight_offset = idx * HIDDEN;
+
+    let mut i = 0;
+    while i < HIDDEN {
+        unsafe {
+            let in_feat_ptr = input_features.get_unchecked(i);
+            let out_feat_ptr = output_features.get_unchecked_mut(i);
+            let weight = *weights.get_unchecked(i + weight_offset);
+            *out_feat_ptr = in_feat_ptr.wrapping_add(weight);
+        }
+        i += 1;
+    }
+}
+
+#[inline]
+pub fn sub(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    sub: Feature,
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let idx = sub.index(perspective, mirror);
+    let weight_offset = idx * HIDDEN;
+
+    let mut i = 0;
+    while i < HIDDEN {
+        unsafe {
+            let in_feat_ptr = input_features.get_unchecked(i);
+            let out_feat_ptr = output_features.get_unchecked_mut(i);
+            let weight = *weights.get_unchecked(i + weight_offset);
+            *out_feat_ptr = in_feat_ptr.wrapping_sub(weight);
+        }
+        i += 1;
+    }
+}
+
+#[inline]
+pub fn add_sub(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    add: Feature,
+    sub: Feature,
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let add_offset = add.index(perspective, mirror) * HIDDEN;
+    let sub_offset = sub.index(perspective, mirror) * HIDDEN;
+
+    let mut i = 0;
+    while i < HIDDEN {
+        unsafe {
+            let in_feat_ptr = input_features.get_unchecked(i);
+            let out_feat_ptr = output_features.get_unchecked_mut(i);
+            *out_feat_ptr = in_feat_ptr
+                .wrapping_add(*weights.get_unchecked(i + add_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub_offset));
+        }
+        i += 1;
+    }
+}
+
+#[inline]
+#[allow(clippy::too_many_arguments)]
+pub fn add_sub_sub(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    add: Feature,
+    sub1: Feature,
+    sub2: Feature,
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let add_offset = add.index(perspective, mirror) * HIDDEN;
+    let sub1_offset = sub1.index(perspective, mirror) * HIDDEN;
+    let sub2_offset = sub2.index(perspective, mirror) * HIDDEN;
+
+    let mut i = 0;
+    while i < HIDDEN {
+        unsafe {
+            let in_feat_ptr = input_features.get_unchecked(i);
+            let out_feat_ptr = output_features.get_unchecked_mut(i);
+            *out_feat_ptr = in_feat_ptr
+                .wrapping_add(*weights.get_unchecked(i + add_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub1_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub2_offset));
+        }
+        i += 1;
+    }
+}
+
+#[inline]
+#[allow(clippy::too_many_arguments)]
+pub fn add_add_sub_sub(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    add1: Feature,
+    add2: Feature,
+    sub1: Feature,
+    sub2: Feature,
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let add1_offset = add1.index(perspective, mirror) * HIDDEN;
+    let add2_offset = add2.index(perspective, mirror) * HIDDEN;
+    let sub1_offset = sub1.index(perspective, mirror) * HIDDEN;
+    let sub2_offset = sub2.index(perspective, mirror) * HIDDEN;
+
+    let mut i = 0;
+    while i < HIDDEN {
+        unsafe {
+            let in_feat_ptr = input_features.get_unchecked(i);
+            let out_feat_ptr = output_features.get_unchecked_mut(i);
+            *out_feat_ptr = in_feat_ptr
+                .wrapping_add(*weights.get_unchecked(i + add1_offset))
+                .wrapping_add(*weights.get_unchecked(i + add2_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub1_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub2_offset));
+        }
+        i += 1;
+    }
+}
+
+#[inline]
+#[allow(clippy::too_many_arguments)]
+pub fn add_add_add_add(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    add1: Feature,
+    add2: Feature,
+    add3: Feature,
+    add4: Feature,
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let add1_offset = add1.index(perspective, mirror) * HIDDEN;
+    let add2_offset = add2.index(perspective, mirror) * HIDDEN;
+    let add3_offset = add3.index(perspective, mirror) * HIDDEN;
+    let add4_offset = add4.index(perspective, mirror) * HIDDEN;
+
+    let mut i = 0;
+    while i < HIDDEN {
+        unsafe {
+            let in_feat_ptr = input_features.get_unchecked(i);
+            let out_feat_ptr = output_features.get_unchecked_mut(i);
+            *out_feat_ptr = in_feat_ptr
+                .wrapping_add(*weights.get_unchecked(i + add1_offset))
+                .wrapping_add(*weights.get_unchecked(i + add2_offset))
+                .wrapping_add(*weights.get_unchecked(i + add3_offset))
+                .wrapping_add(*weights.get_unchecked(i + add4_offset));
+        }
+        i += 1;
+    }
+}
+
+#[inline]
+#[allow(clippy::too_many_arguments)]
+pub fn sub_sub_sub_sub(
+    input_features: &[i16; HIDDEN],
+    output_features: &mut [i16; HIDDEN],
+    sub1: Feature,
+    sub2: Feature,
+    sub3: Feature,
+    sub4: Feature,
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let sub1_offset = sub1.index(perspective, mirror) * HIDDEN;
+    let sub2_offset = sub2.index(perspective, mirror) * HIDDEN;
+    let sub3_offset = sub3.index(perspective, mirror) * HIDDEN;
+    let sub4_offset = sub4.index(perspective, mirror) * HIDDEN;
+
+    let mut i = 0;
+    while i < HIDDEN {
+        unsafe {
+            let in_feat_ptr = input_features.get_unchecked(i);
+            let out_feat_ptr = output_features.get_unchecked_mut(i);
+            *out_feat_ptr = in_feat_ptr
+                .wrapping_sub(*weights.get_unchecked(i + sub1_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub2_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub3_offset))
+                .wrapping_sub(*weights.get_unchecked(i + sub4_offset));
+        }
+        i += 1;
     }
 }
