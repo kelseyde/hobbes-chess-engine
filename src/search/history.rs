e66b320f616bc252d6b9a6534c6bf127cd7436c2
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-11-18 23:03:35 +0100

    capthist threats bench 1894988

--- src/search/history.rs
+++ src/search/history.rs
@@ -2,7 +2,6 @@ use crate::board::bitboard::Bitboard;
 use crate::board::moves::Move;
 use crate::board::piece::Piece;
 use crate::board::side::Side;
-use crate::board::square::Square;
 use crate::board::Board;
 use crate::search::parameters::{
     capt_hist_bonus_max, capt_hist_bonus_offset, capt_hist_bonus_scale, capt_hist_malus_max,
@@ -24,7 +23,7 @@ pub struct QuietHistory {
 }
 
 pub struct CaptureHistory {
-    entries: Box<[PieceToHistory<[i16; 6]>; 2]>,
+    entries: Box<[[[PieceToHistory<[i16; 6]>; 2]; 2]; 2]>,
 }
 
 pub struct ContinuationHistory {
@@ -51,7 +50,7 @@ impl Histories {
         captured: Option<Piece>,
     ) -> i32 {
         if let Some(captured) = captured {
-            self.capture_history_score(board, mv, pc, captured)
+            self.capture_history_score(board, mv, pc, captured, threats)
         } else {
             self.quiet_history_score(board, ss, mv, ply, threats)
         }
@@ -86,8 +85,10 @@ impl Histories {
         mv: &Move,
         pc: Piece,
         captured: Piece,
+        threats: Bitboard,
     ) -> i32 {
-        self.capture_history.get(board.stm, pc, mv.to(), captured) as i32
+        self.capture_history
+            .get(board.stm, *mv, pc, captured, threats) as i32
     }
 
     pub fn update_continuation_history(
@@ -141,7 +142,7 @@ impl Default for ContinuationHistory {
 }
 
 impl QuietHistory {
-    const MAX: i16 = 16384;
+    const MAX: i32 = 16384;
 
     pub fn get(&self, stm: Side, mv: Move, threats: Bitboard) -> i16 {
         let threat_index = ThreatIndex::new(mv, threats);
@@ -152,7 +153,7 @@ impl QuietHistory {
         let threat_index = ThreatIndex::new(*mv, threats);
         let entry =
             &mut self.entries[stm][threat_index.from()][threat_index.to()][mv.from()][mv.to()];
-        *entry = gravity(*entry as i32, bonus as i32, Self::MAX as i32) as i16;
+        *entry = gravity(*entry, bonus as i32, Self::MAX);
     }
 
     pub fn clear(&mut self) {
@@ -163,22 +164,33 @@ impl QuietHistory {
 impl CaptureHistory {
     const MAX: i16 = 16384;
 
-    pub fn get(&self, stm: Side, pc: Piece, sq: Square, captured: Piece) -> i16 {
-        self.entries[stm][pc][sq][captured]
+    pub fn get(&self, stm: Side, mv: Move, pc: Piece, captured: Piece, threats: Bitboard) -> i16 {
+        let threat_index = ThreatIndex::new(mv, threats);
+        self.entries[stm][threat_index.from()][threat_index.to()][pc][mv.to()][captured]
     }
 
-    pub fn update(&mut self, stm: Side, pc: Piece, sq: Square, captured: Piece, bonus: i16) {
-        let entry = &mut self.entries[stm][pc][sq][captured];
-        *entry = gravity(*entry as i32, bonus as i32, Self::MAX as i32) as i16;
+    pub fn update(
+        &mut self,
+        stm: Side,
+        mv: Move,
+        pc: Piece,
+        captured: Piece,
+        threats: Bitboard,
+        bonus: i16,
+    ) {
+        let threat_index = ThreatIndex::new(mv, threats);
+        let entry =
+            &mut self.entries[stm][threat_index.from()][threat_index.to()][pc][mv.to()][captured];
+        *entry = gravity(*entry, bonus as i32, Self::MAX as i32);
     }
 
     pub fn clear(&mut self) {
-        self.entries = Box::new([[[[0; 6]; 64]; 6], [[[0; 6]; 64]; 6]]);
+        self.entries = Box::new([[[[[[0; 6]; 64]; 6]; 2]; 2]; 2]);
     }
 }
 
 impl ContinuationHistory {
-    const MAX: i16 = 16384;
+    const MAX: i32 = 16384;
 
     pub fn get(&self, prev_mv: Move, prev_pc: Piece, mv: &Move, pc: Piece) -> i16 {
         self.entries[prev_pc][prev_mv.to()][pc][mv.to()]
@@ -186,7 +198,7 @@ impl ContinuationHistory {
 
     pub fn update(&mut self, prev_mv: &Move, prev_pc: Piece, mv: &Move, pc: Piece, bonus: i16) {
         let entry = &mut self.entries[prev_pc][prev_mv.to()][pc][mv.to()];
-        *entry = gravity(*entry as i32, bonus as i32, Self::MAX as i32) as i16;
+        *entry = gravity(*entry, bonus as i32, Self::MAX);
     }
 
     pub fn clear(&mut self) {
@@ -289,6 +301,7 @@ fn history_malus(depth: i32, scale: i16, offset: i16, max: i16) -> i16 {
     -(scale * depth as i16 - offset).min(max)
 }
 
-fn gravity(current: i32, update: i32, max: i32) -> i32 {
-    current + update - current * update.abs() / max
+fn gravity(current: i16, update: i32, max: i32) -> i16 {
+    let update = update.clamp(-max, max);
+    current + (update - update.abs() * current as i32 / max) as i16
 }
