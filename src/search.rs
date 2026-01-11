ee3ac9287f4d48ef1b2a46f1985ccd2805fc7b49
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-01-11 11:45:06 +0100

    pvs fail emdium bench 1474534

--- src/search.rs
+++ src/search.rs
@@ -761,6 +761,10 @@ fn alpha_beta(board: &Board,
         };
     }
 
+     if !root_node && best_score >= beta && !Score::is_mate(best_score) && !Score::is_mate(alpha) {
+        best_score = (best_score * depth + beta) / (depth + 1);
+    }
+
     // Update static eval correction history.
     if !in_check
         && !singular_search
