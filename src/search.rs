b20d3ee1f745cdd9c854e5562ccf0f5f7f4611a2
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-01-13 18:56:33 +0100

    nmp margin cutnode bench 1349556

--- src/search.rs
+++ src/search.rs
@@ -323,7 +323,7 @@ fn alpha_beta(board: &Board,
         // Null Move Pruning
         // Skip nodes where giving the opponent an extra move (making a 'null move') still fails high.
         if depth >= nmp_min_depth()
-            && static_eval >= beta + nmp_margin()
+            && static_eval >= beta + nmp_margin() - (25 * cut_node as i32)
             && ply as i32 > td.nmp_min_ply
             && board.has_non_pawns()
             && tt_flag != Upper {
