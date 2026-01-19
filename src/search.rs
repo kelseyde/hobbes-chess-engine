ac72b0c28103a6fec861202ca1e3c023f4557306
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-01-19 14:03:04 +0100

    lmr failhighs bench 988169

--- src/search.rs
+++ src/search.rs
@@ -575,7 +575,7 @@ fn alpha_beta<NODE: NodeType>(
             r -= is_quiet as i32 * ((history_score - lmr_hist_offset()) / lmr_hist_divisor()) * 1024;
             r -= !is_quiet as i32 * captured.map_or(0, |c| see::value(c) / lmr_mvv_divisor());
             r += (is_quiet && !see::see(original_board, &mv, 0)) as i32 * lmr_quiet_see();
-            r += td.stack[ply + 1].fail_highs as i32 * 24 * 24;
+            r += (td.stack[ply + 1].fail_highs > 1) as i32 * 1024;
             let reduced_depth = (new_depth - (r / 1024)).clamp(1, new_depth);
 
             // For moves eligible for reduction, we apply the reduction and search with a null window.
