f352b5e55d3580cf44775308256a0619be8dc9ff
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-01-26 13:04:07 +0100

    WIP

--- src/evaluation/multilayer.rs
+++ src/evaluation/multilayer.rs
@@ -13,15 +13,27 @@ const fn mulhi(a: i16, b: i16) -> i16 {
     (product >> 16) as i16
 }
 
+/// We are in [0, 255] space, we want to end up in [0, 64] space for int multilayer.
+/// Our L1 weights are in [-128, 127] space.
+///
 pub fn activate_ft(acc: &Accumulator) -> [u8; L1_SIZE] {
     let mut output = [0; L1_SIZE];
     for side in [White, Black] {
         let input = acc.features(side);
         for i in 0..L1_SIZE / 2 {
-            let left_clamped = input[i].clamp(0, FT_QUANT as i16);
-            let right_clamped = input[i + L1_SIZE / 2].clamp(0, FT_QUANT as i16);
-            let multiplied = (left_clamped as i32) * (right_clamped as i32);
-            let output_val = (multiplied >> FT_SHIFT) as u8;
+            // Load the pair of inputs to be multiplied
+            let left: i16 = input[i];
+            let right: i16 = input[i + L1_SIZE / 2];
+
+            // Clamp inputs to [0, 255] space
+            let left_clamped: u8 = left.clamp(0, FT_QUANT as i16) as u8;
+            let right_clamped: u8 = right.clamp(0, FT_QUANT as i16) as u8;
+
+            // Pairwise multiplication of left and right input
+            let multiplied: i32 = (left_clamped as i32) * (right_clamped as i32);
+            // Culminating in a right shift by 9 back down into [0, 127] space
+            // Note: this is equivalent to the << 7 >> 16 that mulhi does.
+            let output_val: u8 = (multiplied >> FT_SHIFT) as u8;
             output[i + side as usize * L1_SIZE / 2] = output_val;
         }
     }
