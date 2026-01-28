62d6743bc45b38212c4f7064ca4c0a3c908498a2
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-01-28 21:33:05 +0100

    WIP

--- src/evaluation/multilayer.rs
+++ src/evaluation/multilayer.rs
@@ -1,6 +1,7 @@
 use crate::board::Board;
 use crate::board::side::Side::{Black, White};
 use crate::evaluation::accumulator::Accumulator;
+use crate::evaluation::network::HIDDEN;
 
 const kFtQBits: i32 = 8;
 const kL1QBits: i32 = 7;
@@ -20,30 +21,40 @@ const L3_SIZE: u32 = 32;
 const SCALE: i32 = 400;
 
 pub fn forward(acc: &Accumulator, board: &Board) -> i32 {
+    let us = match board.stm {
+        White => &acc.white_features,
+        Black => &acc.black_features,
+    };
+    let them = match board.stm {
+        White => &acc.black_features,
+        Black => &acc.white_features,
+    };
+    let activated_ft = activate_ft(us, them);
+
     0
 }
 
 /// We are in [0, 255] space, we want to end up in [0, 64] space for int multilayer.
 /// Our L1 weights are in [-128, 127] space.
-pub fn activate_ft(acc: &Accumulator) -> [u8; L1_SIZE] {
+pub fn activate_ft(us: &[i16; HIDDEN], them: &[i16; HIDDEN]) -> [u8; L1_SIZE] {
     let mut output = [0; L1_SIZE];
-    for side in [White, Black] {
-        let input = acc.features(side);
+    for (side, feats) in [us, them].into_iter().enumerate() {
         for i in 0..L1_SIZE / 2 {
             // Load the pair of inputs to be multiplied
-            let l = input[i];
-            let r = input[i + L1_SIZE / 2];
+            let left = feats[i];
+            let right = feats[i + L1_SIZE / 2];
 
             // Clamp inputs to [0, 255] space
-            let l_cl = l.clamp(0, FT_QUANT as i16) as u8;
-            let r_cl = r.clamp(0, FT_QUANT as i16) as u8;
+            let l_clamped = left.clamp(0, FT_QUANT as i16) as u8;
+            let r_clamped = right.clamp(0, FT_QUANT as i16) as u8;
 
             // Pairwise multiplication of left and right input
-            let mul: i32 = (l_cl as i32) * (r_cl as i32);
+            let multiplied: i32 = (l_clamped as i32) * (r_clamped as i32);
+
             // Culminating in a right shift by 9 back down into [0, 127] space
             // Note: this is equivalent to the << 7 >> 16 that mulhi does.
-            let out: u8 = (mul >> FT_SHIFT) as u8;
-            output[i + side as usize * L1_SIZE / 2] = out;
+            let result: u8 = (multiplied >> FT_SHIFT) as u8;
+            output[i + side * L1_SIZE / 2] = result;
         }
     }
     output
