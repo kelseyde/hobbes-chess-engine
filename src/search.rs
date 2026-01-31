81d3bb466ee4b0b338d963511f6c91a29cd68336
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-01-31 12:17:55 +0100

    bench 1645886

--- src/search.rs
+++ src/search.rs
@@ -1058,17 +1058,7 @@ fn calc_improvement(td: &ThreadData, ply: usize, static_eval: i32, in_check: boo
 }
 
 fn late_move_threshold(depth: i32, improving: bool) -> i32 {
-    let base = if improving {
-        lmp_improving_base()
-    } else {
-        lmp_base()
-    };
-    let scale = if improving {
-        lmp_improving_scale()
-    } else {
-        lmp_scale()
-    };
-    (base + depth * scale) / 10
+    (3 + depth * depth) / (2 - improving as i32)
 }
 
 fn print_search_info(board: &Board, td: &mut ThreadData) {
