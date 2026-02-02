ef939c8afdef584d59f2e94e0b16d9da3506f854
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-02-02 23:50:17 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use crate::tools::uci::UCI;
 use board::ray;
-use crate::board::Board;
-use crate::evaluation::NNUE;
 
 pub const AUTHOR: &str = "Dan Kelsey";
 pub const CONTRIBUTORS: &str = "Jonathan Hallström, Mattia Giambirtone";
@@ -24,39 +22,10 @@ pub mod search;
 pub mod tools;
 
 fn main() {
-
-    let nnue = &mut NNUE::default();
-    let board = Board::new();
-    nnue.activate(&board);
-    let eval = nnue.evaluate(&board);
-    println!("Initial position evaluation: {}", eval);
-    // let mut correct = 0;
-    //
-    // correct += check_eval("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 90, nnue) as i32;
-    // correct += check_eval("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", -39, nnue) as i32;
-    // correct += check_eval("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 654, nnue) as i32;
-    // correct += check_eval("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/P2P2PP/q2Q1R1K w kq - 0 2", -903, nnue) as i32;
-    // correct += check_eval("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 32, nnue) as i32;
-    // correct += check_eval("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 288, nnue) as i32;
-    // correct += check_eval("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNB1KBNR w KQkq - 0 1", -903, nnue) as i32;
-    // correct += check_eval("rnb1kbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 748, nnue) as i32;
-    //
-    // if correct != 8 {
-    //     panic!("{} / 8 evaluations correct", correct);
-    // }
-
     // Initialise static data
-    // ray::init();
+    ray::init();
 
-    // // Start up the UCI (Universal Chess Interface)
-    // let args: Vec<String> = std::env::args().collect();
-    // UCI::new().run(&args);
-}
-
-fn check_eval(fen: &str, expected: i32, nnue: &mut NNUE) -> bool {
-    let board = board::Board::from_fen(fen).unwrap();
-    nnue.activate(&board);
-    let eval = nnue.evaluate(&board);
-    println!("expected: {}, actual: {}", expected, eval);
-    eval == expected
+    // Start up the UCI (Universal Chess Interface)
+    let args: Vec<String> = std::env::args().collect();
+    UCI::new().run(&args);
 }
