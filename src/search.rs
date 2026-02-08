3ac668aef04ae1f11d97e16c2110d8ad0ccff514
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-02-08 12:42:23 +0100

    bench 1399934

--- src/search.rs
+++ src/search.rs
@@ -587,6 +587,7 @@ fn alpha_beta<NODE: NodeType>(
             r -= lmr_ttpv_tt_score() * (tt_pv && has_tt_score && tt_score > alpha) as i32;
             r -= lmr_ttpv_tt_depth() * (tt_pv && has_tt_score && tt_depth >= depth) as i32;
             r += lmr_cut_node() * cut_node as i32;
+            r -= 1024 * in_check as i32;
             r -= lmr_capture() * captured.is_some() as i32;
             r += lmr_improving() * !improving as i32;
             r -= lmr_shallow() * (depth == lmr_min_depth()) as i32;
