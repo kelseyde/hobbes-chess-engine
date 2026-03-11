997bac613a62d87cbd8fe0ba560247b361388ee3
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-03-11 10:37:13 +0100

    bench 2340906

--- src/search.rs
+++ src/search.rs
@@ -357,7 +357,7 @@ fn alpha_beta<NODE: NodeType>(
         // Null Move Pruning
         // Skip nodes where giving the opponent an extra move (making a 'null move') still fails high.
         if depth >= nmp_min_depth()
-            && static_eval >= beta + nmp_margin()
+            && static_eval >= beta + nmp_margin() - 128 * improvement / 1024
             && ply as i32 > td.nmp_min_ply
             && board.has_non_pawns()
             && tt_flag != Upper {
