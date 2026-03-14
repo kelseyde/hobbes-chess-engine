d7ecf0afa0185a38552a23e2a27c3da5e6a1af93
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-03-14 13:33:21 +0100

    bench 2386454

--- src/search.rs
+++ src/search.rs
@@ -447,7 +447,7 @@ fn alpha_beta<NODE: NodeType>(
         let is_killer = td.stack[ply].killer.is_some_and(|k| k == mv);
         let history_score = td.history.history_score(board, &td.stack, &mv, ply, threats, pc, captured);
         let base_reduction = td.lmr.reduction(depth, legal_moves, is_quiet);
-        let lmr_depth = depth.saturating_sub(base_reduction);
+        let lmr_depth = depth.saturating_sub(base_reduction).saturating_sub(!improving as i32);
 
         let mut extension = 0;
 
