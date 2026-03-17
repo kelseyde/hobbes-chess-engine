408606cf3bb4167a060325aa815b98342acc7095
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-03-17 17:11:47 +0100

    bench 2106630

--- src/search.rs
+++ src/search.rs
@@ -232,13 +232,13 @@ fn alpha_beta<NODE: NodeType>(
     // best move, score, and static eval - to inform the current search.
     if !singular_search {
         if let Some(entry) = td.tt.probe(board.hash()) {
-            tt_hit = true;
             tt_score = entry.score(ply) as i32;
             tt_eval = entry.static_eval() as i32;
             has_tt_score = Score::is_defined(tt_score);
             tt_depth = entry.depth() as i32;
             tt_flag = entry.flag();
             tt_pv = tt_pv || entry.pv();
+            tt_hit = tt_flag != TTFlag::None;
             if can_use_tt_move(board, &entry.best_move()) {
                 tt_move = entry.best_move();
                 tt_move_noisy = board.is_noisy(&tt_move)
@@ -890,9 +890,9 @@ fn qs(board: &Board, td: &mut ThreadData, mut alpha: i32, beta: i32, ply: usize)
     let mut tt_move = Move::NONE;
     let mut tt_eval = Score::MIN;
     if let Some(entry) = td.tt.probe(board.hash()) {
-        tt_hit = true;
         tt_pv = tt_pv || entry.pv();
         tt_eval = entry.static_eval() as i32;
+        tt_hit = entry.flag() != TTFlag::None;
         if can_use_tt_move(board, &entry.best_move()) {
             tt_move = entry.best_move();
         }
