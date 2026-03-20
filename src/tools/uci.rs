46ae65511583341f02d47d710fd4eca8b0dbf589
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-03-20 15:17:37 +0100

    bench 2574786

--- src/tools/uci.rs
+++ src/tools/uci.rs
@@ -52,18 +52,6 @@ impl UCI {
 
         pretty::print_uci_info();
 
-        let board = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap();
-        self.td.nnue.activate(&board);
-        println!("eval: {}", self.td.nnue.evaluate(&board));
-
-        let board = Board::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1").unwrap();
-        self.td.nnue.activate(&board);
-        println!("eval: {}", self.td.nnue.evaluate(&board));
-
-        let board = Board::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1").unwrap();
-        self.td.nnue.activate(&board);
-        println!("eval: {}", self.td.nnue.evaluate(&board));
-
         loop {
             let mut line = String::new();
             io::stdin()
