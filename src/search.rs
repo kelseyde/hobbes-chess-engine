9bb68a1fa549307a6ae377473dcfcbe986bf0a14
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-04-13 18:38:29 +0200

    bench 2782175

--- src/search.rs
+++ src/search.rs
@@ -541,7 +541,9 @@ fn alpha_beta<NODE: NodeType>(
 
         // Bad Noisy Pruning
         // Skip bad noisies when the static evaluation + some margin is still below alpha.
-        let futility_margin = static_eval + bnp_scale() * lmr_depth;
+        let futility_margin = static_eval
+            + bnp_scale() * lmr_depth
+            + ((history_score - 4000) / 256);
         if !pv_node
             && !in_check
             && lmr_depth < bnp_max_depth()
