1a8304328114291e93f807e866be68cb53d63875
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-04-15 10:32:25 +0200

    bench 2563537

--- src/search.rs
+++ src/search.rs
@@ -505,7 +505,7 @@ fn alpha_beta<NODE: NodeType>(
             + history_score / fp_history_divisor()
             + is_killer as i32 * fp_killer()
             - (tt_hit && tt_flag == Upper) as i32 * fp_tt_upper()
-            - 20 * board.gives_direct_check(mv) as i32;
+            + 20 * board.gives_direct_check(mv) as i32;
         if !root_node
             && !in_check
             && is_quiet
