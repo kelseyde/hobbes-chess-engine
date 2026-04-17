bb5200833c32a2dd27f228cd286cf19eafd4982d
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-04-17 11:27:05 +0200

    bench 2881450

--- src/evaluation/accumulator.rs
+++ src/evaluation/accumulator.rs
@@ -4,6 +4,10 @@ use crate::evaluation::feature::Feature;
 use crate::evaluation::{simd, NETWORK};
 use hobbes_nnue_arch::{FeatureWeights, L1_SIZE};
 
+/// The `Accumulator` holds the pre-activations of the first layer of the neural network. The input
+/// layer just encodes the positions of pieces on the board, from the perspective of both sides.
+/// The accumulator is updated incrementally when a move is made or unmade during search, so that we
+/// can efficiently compute the evaluation without needing to recompute the board state each time.
 #[derive(Clone, Copy)]
 #[repr(C, align(64))]
 pub struct Accumulator {
@@ -15,6 +19,10 @@ pub struct Accumulator {
     pub mirrored: [bool; 2],
 }
 
+/// A single update to the `Accumulator` caused by a move. A standard move will add one feature (the
+/// piece moving to its new square) and remove one feature (the piece leaving its old square).
+/// Captures require one extra feature (removing the captured piece), and castling requires two extra
+/// features (moving the rook).
 #[derive(Clone, Copy, Default)]
 pub struct AccumulatorUpdate {
     pub add_count: usize,
@@ -75,6 +83,8 @@ impl AccumulatorUpdate {
 }
 
 impl Accumulator {
+
+    /// Get a reference to the features for the given perspective.
     #[inline(always)]
     pub fn features(&self, perspective: Side) -> &[i16; L1_SIZE] {
         match perspective {
@@ -83,6 +93,7 @@ impl Accumulator {
         }
     }
 
+    /// Get a mutable reference to the features for the given perspective.
     #[inline(always)]
     pub fn features_mut(&mut self, perspective: Side) -> &mut [i16; L1_SIZE] {
         match perspective {
@@ -91,6 +102,7 @@ impl Accumulator {
         }
     }
 
+    /// Reset the features for the given perspective to the initial biases.
     #[inline]
     pub fn reset(&mut self, perspective: Side) {
         let feats = match perspective {
@@ -100,6 +112,7 @@ impl Accumulator {
         *feats = NETWORK.l0_biases;
     }
 
+    /// Copy the features from another accumulator into this one, for the given perspective.
     #[inline]
     pub fn copy_from(&mut self, side: Side, features: &[i16; L1_SIZE]) {
         match side {
@@ -108,61 +121,20 @@ impl Accumulator {
         }
     }
 
+    /// Get a mutable and immutable reference to the features for the given perspective.
     #[inline]
-    pub fn add(&mut self, add: Feature, weights: &FeatureWeights, perspective: Side) {
-        let mirror = self.mirrored[perspective as usize];
-        let feats  = self.features_mut(perspective).as_mut_ptr();
-        let wa     = unsafe { weights.as_ptr().add(add.index(perspective, mirror) * L1_SIZE) };
-        unsafe { update_features::<1, 0>(feats, feats, [wa], []) };
-    }
-
-    #[inline]
-    pub fn sub(&mut self, sub: Feature, weights: &FeatureWeights, perspective: Side) {
-        let mirror = self.mirrored[perspective as usize];
-        let feats  = self.features_mut(perspective).as_mut_ptr();
-        let ws     = unsafe { weights.as_ptr().add(sub.index(perspective, mirror) * L1_SIZE) };
-        unsafe { update_features::<0, 1>(feats, feats, [], [ws]) };
-    }
-
-    #[inline]
-    pub fn add_add_add_add(
-        &mut self,
-        add1: Feature,
-        add2: Feature,
-        add3: Feature,
-        add4: Feature,
-        weights: &FeatureWeights,
-        perspective: Side,
-    ) {
-        let mirror = self.mirrored[perspective as usize];
-        let feats  = self.features_mut(perspective).as_mut_ptr();
-        let wa1    = unsafe { weights.as_ptr().add(add1.index(perspective, mirror) * L1_SIZE) };
-        let wa2    = unsafe { weights.as_ptr().add(add2.index(perspective, mirror) * L1_SIZE) };
-        let wa3    = unsafe { weights.as_ptr().add(add3.index(perspective, mirror) * L1_SIZE) };
-        let wa4    = unsafe { weights.as_ptr().add(add4.index(perspective, mirror) * L1_SIZE) };
-        unsafe { update_features::<4, 0>(feats, feats, [wa1, wa2, wa3, wa4], []) };
-    }
-
-    #[inline]
-    pub fn sub_sub_sub_sub(
-        &mut self,
-        sub1: Feature,
-        sub2: Feature,
-        sub3: Feature,
-        sub4: Feature,
-        weights: &FeatureWeights,
-        perspective: Side,
-    ) {
-        let mirror = self.mirrored[perspective as usize];
-        let feats  = self.features_mut(perspective).as_mut_ptr();
-        let ws1    = unsafe { weights.as_ptr().add(sub1.index(perspective, mirror) * L1_SIZE) };
-        let ws2    = unsafe { weights.as_ptr().add(sub2.index(perspective, mirror) * L1_SIZE) };
-        let ws3    = unsafe { weights.as_ptr().add(sub3.index(perspective, mirror) * L1_SIZE) };
-        let ws4    = unsafe { weights.as_ptr().add(sub4.index(perspective, mirror) * L1_SIZE) };
-        unsafe { update_features::<0, 4>(feats, feats, [], [ws1, ws2, ws3, ws4]) };
+    pub fn features_inplace(&mut self, side: Side) -> (&[i16; L1_SIZE], &mut [i16; L1_SIZE]) {
+        let p = self.features_mut(side).as_mut_ptr();
+        unsafe {
+            (
+                &*(p as *const [i16; L1_SIZE]),
+                &mut *(p as *mut [i16; L1_SIZE]),
+            )
+        }
     }
 }
 
+/// Apply the given update to the accumulator, modifying the output features in place.
 #[rustfmt::skip]
 pub fn apply_update(
     input_features: &[i16; L1_SIZE],
@@ -175,36 +147,36 @@ pub fn apply_update(
     match update.update_type() {
         AccumulatorUpdateType::None => {}
         AccumulatorUpdateType::Add => {
-            let add1 = unsafe { update.adds[0].unwrap_unchecked() };
-            add(input_features, output_features, add1, weights, perspective, mirror);
+            let add = unsafe { update.adds[0].unwrap_unchecked() };
+            add1(input_features, output_features, add, weights, perspective, mirror);
         }
         AccumulatorUpdateType::Sub => {
-            let sub1 = unsafe { update.subs[0].unwrap_unchecked() };
-            sub(input_features, output_features, sub1, weights, perspective, mirror);
+            let sub = unsafe { update.subs[0].unwrap_unchecked() };
+            sub1(input_features, output_features, sub, weights, perspective, mirror);
         }
         AccumulatorUpdateType::AddSub => {
             let add1 = unsafe { update.adds[0].unwrap_unchecked() };
             let sub1 = unsafe { update.subs[0].unwrap_unchecked() };
-            add_sub(input_features, output_features, add1, sub1, weights, perspective, mirror);
+            add1_sub1(input_features, output_features, add1, sub1, weights, perspective, mirror);
         }
         AccumulatorUpdateType::AddSubSub => {
             let add1 = unsafe { update.adds[0].unwrap_unchecked() };
             let sub1 = unsafe { update.subs[0].unwrap_unchecked() };
             let sub2 = unsafe { update.subs[1].unwrap_unchecked() };
-            add_sub_sub(input_features, output_features, add1, sub1, sub2, weights, perspective, mirror);
+            add1_sub2(input_features, output_features, add1, sub1, sub2, weights, perspective, mirror);
         }
         AccumulatorUpdateType::AddAddSubSub => {
             let add1 = unsafe { update.adds[0].unwrap_unchecked() };
             let add2 = unsafe { update.adds[1].unwrap_unchecked() };
             let sub1 = unsafe { update.subs[0].unwrap_unchecked() };
             let sub2 = unsafe { update.subs[1].unwrap_unchecked() };
-            add_add_sub_sub(input_features, output_features, add1, add2, sub1, sub2, weights, perspective, mirror);
+            add2_sub2(input_features, output_features, add1, add2, sub1, sub2, weights, perspective, mirror);
         }
     }
 }
 
 #[inline]
-pub fn add(
+pub fn add1(
     input_features: &[i16; L1_SIZE],
     output_features: &mut [i16; L1_SIZE],
     add: Feature,
@@ -212,14 +184,14 @@ pub fn add(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr  = input_features.as_ptr();
+    let in_ptr = input_features.as_ptr();
     let out_ptr = output_features.as_mut_ptr();
-    let wa      = weight_ptr(weights, add, perspective, mirror);
+    let wa = weight_ptr(weights, add, perspective, mirror);
     unsafe { update_features::<1, 0>(in_ptr, out_ptr, [wa], []) };
 }
 
 #[inline]
-pub fn sub(
+pub fn sub1(
     input_features: &[i16; L1_SIZE],
     output_features: &mut [i16; L1_SIZE],
     sub: Feature,
@@ -227,14 +199,14 @@ pub fn sub(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr  = input_features.as_ptr();
+    let in_ptr = input_features.as_ptr();
     let out_ptr = output_features.as_mut_ptr();
-    let ws      = weight_ptr(weights, sub, perspective, mirror);
+    let ws = weight_ptr(weights, sub, perspective, mirror);
     unsafe { update_features::<0, 1>(in_ptr, out_ptr, [], [ws]) };
 }
 
 #[inline]
-pub fn add_sub(
+pub fn add1_sub1(
     input_features: &[i16; L1_SIZE],
     output_features: &mut [i16; L1_SIZE],
     add: Feature,
@@ -243,16 +215,16 @@ pub fn add_sub(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr  = input_features.as_ptr();
+    let in_ptr = input_features.as_ptr();
     let out_ptr = output_features.as_mut_ptr();
-    let wa      = weight_ptr(weights, add, perspective, mirror);
-    let ws      = weight_ptr(weights, sub, perspective, mirror);
+    let wa = weight_ptr(weights, add, perspective, mirror);
+    let ws = weight_ptr(weights, sub, perspective, mirror);
     unsafe { update_features::<1, 1>(in_ptr, out_ptr, [wa], [ws]) };
 }
 
 #[inline]
 #[allow(clippy::too_many_arguments)]
-pub fn add_sub_sub(
+pub fn add1_sub2(
     input_features: &[i16; L1_SIZE],
     output_features: &mut [i16; L1_SIZE],
     add: Feature,
@@ -262,17 +234,17 @@ pub fn add_sub_sub(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr  = input_features.as_ptr();
+    let in_ptr = input_features.as_ptr();
     let out_ptr = output_features.as_mut_ptr();
-    let wa      = weight_ptr(weights, add,  perspective, mirror);
-    let ws1     = weight_ptr(weights, sub1, perspective, mirror);
-    let ws2     = weight_ptr(weights, sub2, perspective, mirror);
+    let wa = weight_ptr(weights, add, perspective, mirror);
+    let ws1 = weight_ptr(weights, sub1, perspective, mirror);
+    let ws2 = weight_ptr(weights, sub2, perspective, mirror);
     unsafe { update_features::<1, 2>(in_ptr, out_ptr, [wa], [ws1, ws2]) };
 }
 
 #[inline]
 #[allow(clippy::too_many_arguments)]
-pub fn add_add_sub_sub(
+pub fn add2_sub2(
     input_features: &[i16; L1_SIZE],
     output_features: &mut [i16; L1_SIZE],
     add1: Feature,
@@ -283,17 +255,53 @@ pub fn add_add_sub_sub(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr  = input_features.as_ptr();
+    let in_ptr = input_features.as_ptr();
     let out_ptr = output_features.as_mut_ptr();
-    let wa1     = weight_ptr(weights, add1, perspective, mirror);
-    let wa2     = weight_ptr(weights, add2, perspective, mirror);
-    let ws1     = weight_ptr(weights, sub1, perspective, mirror);
-    let ws2     = weight_ptr(weights, sub2, perspective, mirror);
+    let wa1 = weight_ptr(weights, add1, perspective, mirror);
+    let wa2 = weight_ptr(weights, add2, perspective, mirror);
+    let ws1 = weight_ptr(weights, sub1, perspective, mirror);
+    let ws2 = weight_ptr(weights, sub2, perspective, mirror);
     unsafe { update_features::<2, 2>(in_ptr, out_ptr, [wa1, wa2], [ws1, ws2]) };
 }
 
+#[inline]
+pub fn add4(
+    input_features: &[i16; L1_SIZE],
+    output_features: &mut [i16; L1_SIZE],
+    adds: [Feature; 4],
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let in_ptr = input_features.as_ptr();
+    let out_ptr = output_features.as_mut_ptr();
+    let wa1 = weight_ptr(weights, adds[0], perspective, mirror);
+    let wa2 = weight_ptr(weights, adds[1], perspective, mirror);
+    let wa3 = weight_ptr(weights, adds[2], perspective, mirror);
+    let wa4 = weight_ptr(weights, adds[3], perspective, mirror);
+    unsafe { update_features::<4, 0>(in_ptr, out_ptr, [wa1, wa2, wa3, wa4], []) };
+}
+
+#[inline]
+pub fn sub4(
+    input_features: &[i16; L1_SIZE],
+    output_features: &mut [i16; L1_SIZE],
+    subs: [Feature; 4],
+    weights: &FeatureWeights,
+    perspective: Side,
+    mirror: bool,
+) {
+    let in_ptr = input_features.as_ptr();
+    let out_ptr = output_features.as_mut_ptr();
+    let ws1 = weight_ptr(weights, subs[0], perspective, mirror);
+    let ws2 = weight_ptr(weights, subs[1], perspective, mirror);
+    let ws3 = weight_ptr(weights, subs[2], perspective, mirror);
+    let ws4 = weight_ptr(weights, subs[3], perspective, mirror);
+    unsafe { update_features::<0, 4>(in_ptr, out_ptr, [], [ws1, ws2, ws3, ws4]) };
+}
+
 #[inline(always)]
-unsafe fn update_features<const ADDS: usize, const SUBS: usize>(
+pub unsafe fn update_features<const ADDS: usize, const SUBS: usize>(
     input: *const i16,
     output: *mut i16,
     adds: [*const i16; ADDS],
@@ -317,6 +325,15 @@ unsafe fn update_features<const ADDS: usize, const SUBS: usize>(
 }
 
 #[inline(always)]
-fn weight_ptr(weights: &FeatureWeights, feature: Feature, perspective: Side, mirror: bool) -> *const i16 {
-    unsafe { weights.as_ptr().add(feature.index(perspective, mirror) * L1_SIZE) }
+pub fn weight_ptr(
+    weights: &FeatureWeights,
+    feature: Feature,
+    perspective: Side,
+    mirror: bool,
+) -> *const i16 {
+    unsafe {
+        weights
+            .as_ptr()
+            .add(feature.index(perspective, mirror) * L1_SIZE)
+    }
 }
