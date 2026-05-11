e143e39588f3e74f876dde51e6ce023262098b95
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-05-11 23:58:09 +0200

    bench 1598495

--- src/search.rs
+++ src/search.rs
@@ -1033,7 +1033,8 @@ fn qs(board: &Board, td: &mut ThreadData, mut alpha: i32, beta: i32, ply: usize)
 
         // SEE Pruning
         // Skip moves which lose material once all the pieces are swapped off.
-        if !in_check
+        if !pv_node
+            && !in_check
             && !is_killer
             && threats.contains(mv.to())
             && !see::see(board, &mv, qs_see_threshold(), Pruning)
