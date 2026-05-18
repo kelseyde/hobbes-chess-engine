f49bc748b3c546d6b38457db39a36e07b67408e8
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-05-18 23:06:35 +0200

    bench 1661303

--- src/evaluation/accumulator.rs
+++ src/evaluation/accumulator.rs
@@ -1,7 +1,7 @@
 use crate::board::side::Side;
 use crate::board::side::Side::{Black, White};
 use crate::evaluation::feature::Feature;
-use crate::evaluation::{simd, NETWORK};
+use crate::evaluation::NETWORK;
 use hobbes_nnue_arch::{FeatureWeights, L1_SIZE};
 
 /// The `Accumulator` holds the pre-activations of the first layer of the neural network. The input
@@ -129,10 +129,10 @@ pub fn add1(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr = input_features.as_ptr();
-    let out_ptr = output_features.as_mut_ptr();
-    let wa = weight_ptr(weights, add, perspective, mirror);
-    unsafe { update_features::<1, 0>(in_ptr, out_ptr, [wa], []) };
+    let wa = weight_slice(weights, add, perspective, mirror);
+    for i in 0..L1_SIZE {
+        output_features[i] = input_features[i] + wa[i];
+    }
 }
 
 #[inline]
@@ -144,10 +144,10 @@ pub fn sub1(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr = input_features.as_ptr();
-    let out_ptr = output_features.as_mut_ptr();
-    let ws = weight_ptr(weights, sub, perspective, mirror);
-    unsafe { update_features::<0, 1>(in_ptr, out_ptr, [], [ws]) };
+    let ws = weight_slice(weights, sub, perspective, mirror);
+    for i in 0..L1_SIZE {
+        output_features[i] = input_features[i] - ws[i];
+    }
 }
 
 #[inline]
@@ -160,11 +160,11 @@ pub fn add1_sub1(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr = input_features.as_ptr();
-    let out_ptr = output_features.as_mut_ptr();
-    let wa = weight_ptr(weights, add, perspective, mirror);
-    let ws = weight_ptr(weights, sub, perspective, mirror);
-    unsafe { update_features::<1, 1>(in_ptr, out_ptr, [wa], [ws]) };
+    let wa = weight_slice(weights, add, perspective, mirror);
+    let ws = weight_slice(weights, sub, perspective, mirror);
+    for i in 0..L1_SIZE {
+        output_features[i] = input_features[i] + wa[i] - ws[i];
+    }
 }
 
 #[inline]
@@ -179,12 +179,12 @@ pub fn add1_sub2(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr = input_features.as_ptr();
-    let out_ptr = output_features.as_mut_ptr();
-    let wa = weight_ptr(weights, add, perspective, mirror);
-    let ws1 = weight_ptr(weights, sub1, perspective, mirror);
-    let ws2 = weight_ptr(weights, sub2, perspective, mirror);
-    unsafe { update_features::<1, 2>(in_ptr, out_ptr, [wa], [ws1, ws2]) };
+    let wa  = weight_slice(weights, add,  perspective, mirror);
+    let ws1 = weight_slice(weights, sub1, perspective, mirror);
+    let ws2 = weight_slice(weights, sub2, perspective, mirror);
+    for i in 0..L1_SIZE {
+        output_features[i] = input_features[i] + wa[i] - ws1[i] - ws2[i];
+    }
 }
 
 #[inline]
@@ -200,13 +200,13 @@ pub fn add2_sub2(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr = input_features.as_ptr();
-    let out_ptr = output_features.as_mut_ptr();
-    let wa1 = weight_ptr(weights, add1, perspective, mirror);
-    let wa2 = weight_ptr(weights, add2, perspective, mirror);
-    let ws1 = weight_ptr(weights, sub1, perspective, mirror);
-    let ws2 = weight_ptr(weights, sub2, perspective, mirror);
-    unsafe { update_features::<2, 2>(in_ptr, out_ptr, [wa1, wa2], [ws1, ws2]) };
+    let wa1 = weight_slice(weights, add1, perspective, mirror);
+    let wa2 = weight_slice(weights, add2, perspective, mirror);
+    let ws1 = weight_slice(weights, sub1, perspective, mirror);
+    let ws2 = weight_slice(weights, sub2, perspective, mirror);
+    for i in 0..L1_SIZE {
+        output_features[i] = input_features[i] + wa1[i] + wa2[i] - ws1[i] - ws2[i];
+    }
 }
 
 #[inline]
@@ -218,13 +218,13 @@ pub fn add4(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr = input_features.as_ptr();
-    let out_ptr = output_features.as_mut_ptr();
-    let wa1 = weight_ptr(weights, adds[0], perspective, mirror);
-    let wa2 = weight_ptr(weights, adds[1], perspective, mirror);
-    let wa3 = weight_ptr(weights, adds[2], perspective, mirror);
-    let wa4 = weight_ptr(weights, adds[3], perspective, mirror);
-    unsafe { update_features::<4, 0>(in_ptr, out_ptr, [wa1, wa2, wa3, wa4], []) };
+    let wa1 = weight_slice(weights, adds[0], perspective, mirror);
+    let wa2 = weight_slice(weights, adds[1], perspective, mirror);
+    let wa3 = weight_slice(weights, adds[2], perspective, mirror);
+    let wa4 = weight_slice(weights, adds[3], perspective, mirror);
+    for i in 0..L1_SIZE {
+        output_features[i] = input_features[i] + wa1[i] + wa2[i] + wa3[i] + wa4[i];
+    }
 }
 
 #[inline]
@@ -236,49 +236,22 @@ pub fn sub4(
     perspective: Side,
     mirror: bool,
 ) {
-    let in_ptr = input_features.as_ptr();
-    let out_ptr = output_features.as_mut_ptr();
-    let ws1 = weight_ptr(weights, subs[0], perspective, mirror);
-    let ws2 = weight_ptr(weights, subs[1], perspective, mirror);
-    let ws3 = weight_ptr(weights, subs[2], perspective, mirror);
-    let ws4 = weight_ptr(weights, subs[3], perspective, mirror);
-    unsafe { update_features::<0, 4>(in_ptr, out_ptr, [], [ws1, ws2, ws3, ws4]) };
-}
-
-#[inline(always)]
-pub unsafe fn update_features<const ADDS: usize, const SUBS: usize>(
-    input: *const i16,
-    output: *mut i16,
-    adds: [*const i16; ADDS],
-    subs: [*const i16; SUBS],
-) {
-    let mut i = 0;
-    while i + 4 * simd::I16_LANES <= L1_SIZE {
-        for k in 0..4 {
-            let off = i + k * simd::I16_LANES;
-            let mut val = simd::load_i16(input.add(off));
-            for &a in adds.iter() {
-                val = simd::add_i16(val, simd::load_i16(a.add(off)));
-            }
-            for &s in subs.iter() {
-                val = simd::sub_i16(val, simd::load_i16(s.add(off)));
-            }
-            simd::store_i16(output.add(off), val);
-        }
-        i += 4 * simd::I16_LANES;
+    let ws1 = weight_slice(weights, subs[0], perspective, mirror);
+    let ws2 = weight_slice(weights, subs[1], perspective, mirror);
+    let ws3 = weight_slice(weights, subs[2], perspective, mirror);
+    let ws4 = weight_slice(weights, subs[3], perspective, mirror);
+    for i in 0..L1_SIZE {
+        output_features[i] = input_features[i] - ws1[i] - ws2[i] - ws3[i] - ws4[i];
     }
 }
 
 #[inline(always)]
-pub fn weight_ptr(
+pub fn weight_slice(
     weights: &FeatureWeights,
     feature: Feature,
     perspective: Side,
     mirror: bool,
-) -> *const i16 {
-    unsafe {
-        weights
-            .as_ptr()
-            .add(feature.index(perspective, mirror) * L1_SIZE)
-    }
+) -> &[i16; L1_SIZE] {
+    let offset = feature.index(perspective, mirror) * L1_SIZE;
+    unsafe { &*(weights.as_ptr().add(offset) as *const [i16; L1_SIZE]) }
 }
