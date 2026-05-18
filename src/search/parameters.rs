d5bb3b9c3a25fa0fc2a6462efda39c2fc79867f1
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-05-18 13:52:42 +0200

    bench 1573839

--- src/search/parameters.rs
+++ src/search/parameters.rs
@@ -208,6 +208,8 @@ tunable_params! {
     pcm_factor_base              = 88, 0..=256,            true;
     pcm_deep_search_factor       = 128, 0..=256,           true;
     pcm_deep_search_threshold    = 5, 0..=10,              false;
+    pcm_bad_eval_factor          = 256, 0..=512,           true;
+    pcm_bad_eval_threshold       = 128, 0..=256,           true;
     qs_capt_hist_bonus_scale     = 261, 80..=360,          true;
     qs_capt_hist_bonus_offset    = 8, 0..=200,             true;
     qs_capt_hist_bonus_max       = 1470, 1000..=1600,      true;
