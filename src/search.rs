701181c281771aa6e61a089872245eecccd2a1d2
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-07-20 22:35:13 +0200

    WIP

--- src/search.rs
+++ src/search.rs
@@ -1137,7 +1137,7 @@ fn make_move(
     ply: usize,
 ) {
     td.nnue.update(&mv, pc, captured, board);
-    board.make(&mv, td.nnue.stack[ply].threat_mut());
+    board.make(&mv, td.nnue.stack[td.nnue.current].threat_mut());
     td.stack[ply].mv = Some(mv);
     td.stack[ply].pc = Some(pc);
     td.stack[ply].captured = captured;
