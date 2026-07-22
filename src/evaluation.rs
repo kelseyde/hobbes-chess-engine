1b48bd6107e2624d872513a5af8d21ab9baa4186
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-07-22 08:31:09 +0200

    WIP

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -79,9 +79,12 @@ impl NNUE {
     /// with the pre-activations of L0 stored in the current accumulator. We activate L0 and propagate
     /// through L1, L2, and L3 to get the final output.
     pub fn evaluate(&mut self, board: &Board) -> i32 {
+
+        // Apply any pending updates to the PSQ and threat accumulators.
         psq::apply_lazy_updates(self, board);
         threat::apply_lazy_updates(self, board);
 
+        // Arrange the features of both accumulators in (stm, nstm) order.
         let acc = &self.stack[self.current];
         let (psq_us, psq_them) = (&acc.psq.features(board.stm), &acc.psq.features(!board.stm));
         let (threat_us, threat_them) = (&acc.threat.features(board.stm), &acc.threat.features(!board.stm));
@@ -91,6 +94,7 @@ impl NNUE {
         let mut l2_outputs = [0i32; L3_SIZE];
         let output_bucket = get_output_bucket(board);
 
+        // Pass the features through the network to retrieve the raw eval.
         let raw = unsafe {
             inference::activate_l0(psq_us, threat_us, psq_them, threat_them, &mut l0_outputs);
             #[cfg(feature = "track_l0_activations")]
@@ -100,8 +104,10 @@ impl NNUE {
             inference::propagate_l3(&l2_outputs, output_bucket)
         };
 
+        // Scale the eval and return
         let output = raw as i64 * SCALE / (Q * Q * Q * Q);
         scale_evaluation(board, output as i32)
+
     }
 
     /// Activate the entire board from scratch. This initializes the accumulators based on the
