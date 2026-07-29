3c6893511b60974edfffff9b4174f237fa6f227f
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-07-29 18:58:11 +0200

    bench 1780295

--- src/search.rs
+++ src/search.rs
@@ -1031,6 +1031,7 @@ fn qs(board: &Board, td: &mut ThreadData, mut alpha: i32, beta: i32, ply: usize)
         let is_recapture = board.is_recapture(&mv);
         let is_mate_score = is_mate(best_score);
         let is_killer = td.stack[ply].killer.is_some_and(|k| k == mv);
+        let history_score = td.history.history_score(board, &td.stack, &mv, ply, threats, pc, captured);
 
         // Late Move Pruning
         if !in_check && !is_recapture && !is_killer && !is_mate_score && searched_moves >= 2 {
@@ -1057,6 +1058,13 @@ fn qs(board: &Board, td: &mut ThreadData, mut alpha: i32, beta: i32, ply: usize)
             && threats.contains(mv.to())
             && !see::see(board, &mv, qs_see_threshold(), Pruning)
         {
+            continue;
+        }
+
+        // History pruning
+        if !in_check
+            && !is_killer
+            && history_score <= 12000 {
             continue;
         }
 
