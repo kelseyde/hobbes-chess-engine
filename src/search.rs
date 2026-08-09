1fdb5bab6b57dc58b99cd06ece973281cc121d68
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-08-10 01:16:52 +0200

    bench 1988156

--- src/search.rs
+++ src/search.rs
@@ -473,7 +473,7 @@ fn alpha_beta<NODE: NodeType>(
 
                 let triple_margin = se_text_margin(is_quiet) + se_text_pv_margin() * pv_node as i32;
                 extension += (is_quiet && singular_score < s_beta - triple_margin) as i32;
-            } else if s_beta >= beta {
+            } else if !pv_node && s_beta >= beta {
                 return (s_beta * s_depth + beta) / (s_depth + 1);
             } else if tt_score >= beta {
                 extension = -3 + pv_node as i32;
